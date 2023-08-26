7467fad9de3a421069af9cb6952d6fd894dd951f
Paul Zupan <paul.zupan.13@gmail.com>
2023-08-26 12:06:32 -0700

    Enable wireframe mode

--- src/main.cpp
+++ src/main.cpp
@@ -139,6 +139,8 @@ int main() {
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
   while (!glfwWindowShouldClose(window)) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
       glfwSetWindowShouldClose(window, true);
